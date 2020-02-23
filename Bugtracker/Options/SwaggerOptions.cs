99003376b7a47cbe3c380036f0cfe47bf2809df0
Ali Riza Esenli <aliriza.esenli@hotmail.com>
2020-02-23 21:01:10 +0100

    removed unnecessary files

--- Bugtracker/Options/SwaggerOptions.cs
+++ Bugtracker/Options/SwaggerOptions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Bugtracker.Options
+﻿namespace Bugtracker.Options
 {
     public class SwaggerOptions
     {
