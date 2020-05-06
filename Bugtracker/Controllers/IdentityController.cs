23a830a63b962d378247d762a8a984a66279441c
Ali Riza Esenli <aliriza.esenli@hotmail.com>
2020-05-06 23:55:24 +0200

    created new repository

--- Bugtracker/Controllers/IdentityController.cs
+++ Bugtracker/Controllers/IdentityController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Bugtracker.Contracts.Requests;
 using Bugtracker.Contracts.Responses;
 using Bugtracker.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bugtracker.Controllers
@@ -16,6 +17,7 @@ namespace Bugtracker.Controllers
             _identityService = identityService;
         }
 
+        [AllowAnonymous]
         [HttpPost("api/identity/register")]
         public async Task<IActionResult> Register([FromBody] UserRegistrationRequest request)
         {
@@ -44,6 +46,7 @@ namespace Bugtracker.Controllers
             });
         }
 
+        [AllowAnonymous]
         [HttpPost("api/identity/login")]
         public async Task<IActionResult> Login([FromBody] UserLoginRequest request)
         {
